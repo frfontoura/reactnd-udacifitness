22c12c745434b5705b807670ba3fcadfbc6e5949
unknown <frfontoura@gmail.com>
2018-12-05 16:34:09 -0200

    Configuração do Animated

--- components/Live.js
+++ components/Live.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { Location, Permissions } from 'expo'
 
@@ -11,7 +11,8 @@ export default class Live extends Component {
     state = {
         coords: null,
         status: null,
-        direction: ''
+        direction: '',
+        bounceValue: new Animated.Value(1),
     }
 
     componentDidMount() {
@@ -49,6 +50,13 @@ export default class Live extends Component {
             const newDirection = calculateDirection(coords.heading)
             const { direction, bounceValue } = this.state
 
+            if (newDirection !== direction) {
+                Animated.sequence([
+                    Animated.timing(bounceValue, { duration: 200, toValue: 1.04 }),
+                    Animated.spring(bounceValue, { toValue: 1, friction: 4 })
+                ]).start()
+            }
+
             this.setState(() => ({
                 coords,
                 status: 'granted',
@@ -59,7 +67,7 @@ export default class Live extends Component {
 
     render() {
 
-        const { status, coords, direction } = this.state
+        const { status, coords, direction, bounceValue } = this.state
 
         if (status === null) {
             return <ActivityIndicator style={{ marginTop: 30 }} />
@@ -96,9 +104,9 @@ export default class Live extends Component {
             <View style={styles.container}>
                 <View style={styles.directionContainer}>
                     <Text style={styles.header}>You're heading</Text>
-                    <Text style={styles.direction}>
+                    <Animated.Text style={[styles.direction, { transform: [{ scale: bounceValue }] }]}>
                         {direction}
-                    </Text>
+                    </Animated.Text>
                 </View>
                 <View style={styles.metricContainer}>
                     <View style={styles.metric}>
